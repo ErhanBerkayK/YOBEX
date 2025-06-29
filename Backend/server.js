7342b616de05f281be8e0b643d2244bc67222a45
ErhanBerkayK <149186255+ErhanBerkayK@users.noreply.github.com>
2025-06-29 03:56:49 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,50 +22,44 @@ connectDB();
 
 ///// ROUTES:
 
-/// TUM URUNLERI LISTELEME:
-// http://localhost:8000/api/listAllProducts
+
 const productRoutes = require("./routes/listAllProducts.js");
 app.use("/api/listAllProducts", productRoutes);
 
-/// id ye GORE BELLIRLI BIR URUNU GORUNTULEME:
-// http://localhost:8000/api/getProductById/1
+
 const getProductById = require("./routes/getProductById.js");
 app.use("/api/getProductById", getProductById);
 
-/// KATEGORI LISTELEME:
-// http://localhost:8000/api/listCategories
+
 const categoryRoutes = require("./routes/listCategories.js");
 app.use("/api/listCategories", categoryRoutes);
 
-/// KATEGORIYE GORE URUN FILTRELEME:
-// http://localhost:8000/api/filterProductsByCategory?category=Elektronik
+
 const filterByCategoryRoutes = require("./routes/filterProductsByCategory.js");
 app.use("/api/filterProductsByCategory", filterByCategoryRoutes);
 
-/// FIYAT ARALIGINA GORE URUN FILTRELEME:
-// http://localhost:8000/api/productFilteringByPriceRange?minPrice=100&maxPrice=500
+
 const filterByPriceRoutes = require("./routes/productFilteringByPriceRange.js");
 app.use("/api/productFilteringByPriceRange", filterByPriceRoutes);
 
-/// EMRE ROUTES:
+
 app.use("/api/auth", authRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/checkout", checkoutRoutes);
 
-/// YUSUF ROUTES:
+
 app.use(express.json());
 app.use("/api/cart", cartRoutes);
 
-/// NEW ROUTES FOR FAVORITES, PURCHASE, AND BALANCE:
-// Favorites routes
+
 const favoritesRoutes = require("./routes/favorites.js");
 app.use("/api/favorites", favoritesRoutes);
 
-// Purchase routes
+
 const purchaseRoutes = require("./routes/purchase.js");
 app.use("/api/purchase", purchaseRoutes);
 
-// User balance routes
+
 const userBalanceRoutes = require("./routes/userBalance.js");
 app.use("/api/user/balance", userBalanceRoutes);
 
